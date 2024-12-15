3bb9bf53fc4290562741bf58fb586a732847d381
Helene Villarsin <helene.villarsin@efrei.net>
2024-12-15 14:48:06 +0100

    modification

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/NumberPrinter.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/NumberPrinter.java
@@ -1,2 +1,23 @@
-package com.cleancode.knuth;public class NumberPrinter {
+package com.cleancode.knuth;
+
+public class NumberPrinter {
+    public void print(int[] numbers, int totalNumbers, int rows, int columns) {
+        int pageNumber = 1;
+        int pageOffset = 1;
+
+        while (pageOffset <= totalNumbers) {
+            System.out.printf("The First %d Prime Numbers === Page %d%n%n", totalNumbers, pageNumber);
+            for (int rowOffset = pageOffset; rowOffset <= pageOffset + rows - 1; rowOffset++) {
+                for (int c = 0; c < columns; c++) {
+                    if (rowOffset + c * rows <= totalNumbers) {
+                        System.out.printf("%10d", numbers[rowOffset + c * rows]);
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("\f");
+            pageNumber++;
+            pageOffset += rows * columns;
+        }
+    }
 }
